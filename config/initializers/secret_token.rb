3770dacdb827089f91749bb1317f9f77542ad9ae
Karl Marx <marx@browntown.com>
2012-06-29 22:50:04 +0200

    First ever commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TutApp::Application.config.secret_token = 'f1cf49acc253bbb20bdf771c1c5243dba6788f47cf64ee21339341059c13d8e1c030597c270e7ebe904d93e6628fea33a01a4c9b06d1905533847de34aa4cd30'
