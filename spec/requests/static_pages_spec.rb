fae6237cb8c726e47df5603cbaa5dfb74d4b69c5
Fred J <fred@gitup.com>
2012-07-03 21:48:56 +0200

    Layout and routes finalized

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,50 +1,42 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  subject { page }
 
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the header 'Tutorial'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1',
-                       :text => 'Tutorial')
-    end
+    it { should have_selector('h1', text: 'Tutorial') }
+    
+    it { should have_selector('title', text: 'Rails | Tutorial') }
 
-    it "should have the title 'Tutorial'" do
-      visit '/static_pages/home'
-      page.should have_selector('title',
-                       :text => 'Rails | Tutorial')
-    end
   end
 
   describe "Help page" do
+    before { visit help_path }
 
-    it "should have the header 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1',
-                       :text => 'Help')
-    end
+    it { should have_selector('h1', text: 'Help') }
+    
+    it { should have_selector('title', text: 'Rails | Help') }
 
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('title',
-                       :text => 'Rails | Help')
-    end
   end
 
   describe "About page" do
+    before { visit about_path }
 
-    it "should have the header 'About'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1',
-                       :text => 'About')
-    end
+    it { should have_selector('h1', text: 'About') }
+    
+    it { should have_selector('title', text: 'Rails | About') }
 
-    it "should have the title 'About'" do
-      visit '/static_pages/about'
-      page.should have_selector('title',
-                       :text => 'Rails | About')
-    end
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+
+    it { should have_selector('h1', text: 'Contact') }
+    
+    it { should have_selector('title', text: 'Rails | Contact') }
+
   end
 
 end
